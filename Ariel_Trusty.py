72a9bf85bc5db96bd8c0d0e0c0b7d04b26020f22
ArTrusty <110997809+ArTrusty@users.noreply.github.com>
2025-11-22 23:55:31 -0600

    Add Ariel_Trusty.py contribution

---
+++ Ariel_Trusty.py
@@ -0,0 +1,9 @@
+def my_contribution(): 
+  """ 
+  A simple function by [Your Full Name] 
+  GitHub username: [your-github-username] 
+  """ 
+print("I love GitHub!") 
+print("Submitted by: [Your Full Name]") 
+if __name__ == "__main__": 
+  my_contribution() 
